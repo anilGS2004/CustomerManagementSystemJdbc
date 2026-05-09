b9ab57f158839947b714f01103c5d7c5e9a72e80
anilGS2004 <anilsherkar5@gmail.com>
2026-05-09 23:23:49 +0530

    added new methods in customer class

--- src/main/java/customer_operations/CustomerManagement.java
+++ src/main/java/customer_operations/CustomerManagement.java
@@ -3,26 +3,26 @@ package customer_operations;
 public class CustomerManagement {
 	public static void main(String[] args) {
 		CustomerController cc = new CustomerController();
-//		CustomerPojo cp= cc.getRegisterPage();
-//		String msg= cc.addCustomer(cp);
-//		System.out.println("status : "+msg);
-		// int id = cc.showCustomer();
-		// CustomerPojo cp = cc.showCustomer(id);
+	CustomerPojo cp= cc.getRegisterPage();
+		String msg= cc.addCustomer(cp);
+		System.out.println("status : "+msg);
+		 int id = cc.showCustomer();
+	//	 CustomerPojo cp = cc.showCustomer(id);
 
 //		CustomerPojo cp = cc.updateCustomer();
-//
-//		boolean isUpdated = cc.updateCustomer(cp);
 
-//		if (isUpdated) {
+		boolean isUpdated = cc.updateCustomer(cp);
+
+		if (isUpdated) {
 
 			System.out.println("Customer updated successfully");
 
-//		} else {
-//
-//			System.out.println("Customer not found");
-//		}
+		} else {
 
-		int id = cc.deleteCustomer();
+			System.out.println("Customer not found");
+		}
+
+//		int id = cc.deleteCustomer();
 
 		boolean isDeleted = cc.deleteCustomer(id);
 
